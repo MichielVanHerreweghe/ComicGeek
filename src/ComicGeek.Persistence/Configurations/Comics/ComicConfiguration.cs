ec7fc505b544a7f804dbaa7afc51e42a6b8f99f9
Michiel Van Herreweghe <169037533+MichielVanHerreweghe@users.noreply.github.com>
2025-02-12 09:05:26 +0100

    feat: add persistence layer with ApplicationDbContext and entity configurations

---
+++ src/ComicGeek.Persistence/Configurations/Comics/ComicConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using ComicGeek.Domain.Comics;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ComicGeek.Persistence.Configurations.Comics;
+
+internal sealed class ComicConfiguration : EntityConfiguration<Comic>
+{
+    public override void Configure(EntityTypeBuilder<Comic> builder)
+    {
+        base.Configure(builder);
+    }
+}
